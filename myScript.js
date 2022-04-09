e0bb8d8d965095d9b920576746c36cb756b8a915
ethoswesty <etwest99@gmail.com>
2022-04-09 15:23:31 +0100

    added javascript to display operands on calculator display

--- myScript.js
+++ myScript.js
@@ -1,4 +1,4 @@
-let buttons = document.querySelectorAll("button");
+const buttons = document.querySelector(".calculator_keys");
 let firstOperand = null;
 let secondOperand = null;
 let firstOperator = null;
@@ -6,12 +6,63 @@ let secondOperator = null;
 let result = null;
 let displayValue = "0";
 
+
 function updateDisplay(){
     const display = document.getElementById("display");
     display.innerHTML = displayValue;
 }
 
 updateDisplay();
+
+
+buttons.addEventListener('click', (event) => {
+    const { target } = event;
+    if (!target.matches('button')){
+    return;
+    }
+
+    if (target.classList.contains('operand')){
+        inputOperand(target.value);
+        updateDisplay();
+    }
+
+    if (target.classList.contains('operator')){
+        console.log('operator', target.value);
+    }
+
+    if (target.classList.contains('decimal')){
+        console.log('decimal', target.value);
+    }
+
+    if (target.classList.contains('clear')){
+        console.log('clear', target.value);
+    }
+
+    // inputOperator(target.value);
+    // updateDisplay();
+
+});
+
+function inputOperand(operand){
+    if (firstOperator == null){
+        if(displayValue === "0" || displayValue === 0){
+            displayValue = operand
+        } else if (displayValue === firstOperand){
+            displayValue = operand
+        } else {
+            displayValue += operand
+        }
+    } else {
+        if(displayValue === firstOperand){
+            displayValue = operand
+        } else {
+            displayValue += operand
+        }
+    }
+}
+
+
+
 
 
 function operate(x, y, op){
